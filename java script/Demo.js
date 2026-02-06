3bbda9c3879536f93229d7c273eef88e9131dce1
PriyadharshiniMurugan... <piri29122005@gmail.com>
2026-02-06 11:55:06 +0530

    Add files via upload

---
+++ java script/Demo.js
@@ -0,0 +1,133 @@
+// 1 
+//  var a = 10
+// var b = 20
+// console.log(a+b)
+
+// 2
+// function oppo( ) {
+//     console.log("This is oppo phone")
+// }
+// oppo( )
+
+// 3
+// function iphone( ) {
+//     console.log("This is iphone phone")
+// }
+// iphone( )
+
+// 4
+// function redmi( ) {
+//     console.log("This is redmi phone")
+// }
+// redmi( )
+
+// 5
+// var a = 10
+// var b = 20
+// function add( ) {
+//     console.log(a+b)
+    
+// }
+// add( )
+
+// 6
+
+// var factor = "Suriya"
+// var fplayer = "harthikpandiya"
+// var fmovie = "igloo"
+// function favourite( ) {
+//     console.log("favourite actor:" +factor)
+//     console.log("favourite player:"+fplayer)
+//     console.log("favourite movie:"+fmovie)
+
+// }
+// favourite( )
+// 7
+
+// function area(l,b) {
+
+//     console.log("Area is" + l*b)
+// }
+// area(3,5)
+// 8
+
+// function add(a,b) {
+//     return (a + b)
+// }
+// var add = (10,20)
+// console.log(add)
+
+// 9
+//var rain = false
+// if(rain){
+//     console.log("Take a umbrella")
+// }
+// else{
+//     console.log("Enjoy the sunshine")
+// }
+// // 10
+// var finish = true
+// if(finish){
+
+//     console.log("Greate job")
+// }
+// else{
+//     console.log("Finish your homework before playing")
+// }
+// 11
+// var cookies = true
+// if(cookies){
+//     console.log("Would you like a cookie")
+// }
+// else{
+//     console.log("Time to bake more cookie")
+// }
+// 12
+// var season = "autumn"
+// if(season == "spring"){
+//     console.log("Enjoy the blooming flower.")
+// }
+// if(season == "summer"){
+//     console.log("Have fun in the sun.")
+// }
+// if(season == "autumn"){
+//     console.log("Admire the colorful leaves.")
+// }
+// if(season == "winter"){
+//     console.log("Bundle up and stay warm.")
+// }
+// 13
+// var score = 50
+// if(score < 50){
+//     console.log("You need to improve")
+// }
+// if(score >= 50 && score <= 70){
+//     console.log("Good job")
+// }
+// if(score > 70){
+//     console.log("Excellent performance!")
+// }
+// 14
+// var number = 7
+// if(number%2==0 ){
+    // console.log("The number is even")
+// }
+// else{
+    // console.log("The number is odd")
+// }
+// 15
+// var ch = 'o'
+// if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+//     console.log("its a vowel")
+// }
+// else{
+//     console.log("its a consonant")
+// }
+// 16
+// for(i=1;i<=10;i=i+2){
+//     console.log(i)
+// }
+// 17
+for(i=10;i>=10;i=i=1){
+    console.log(i)
+}
