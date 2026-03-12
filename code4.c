32f172cd99ffdd7b038c2ea77bc317b63d1b6157
PriyadharshiniMurugan... <piri29122005@gmail.com>
2026-03-12 14:18:47 +0530

    Add files via upload

---
+++ code4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[15];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < n; i++) {
+        int count = 0;
+        for(int j = 0; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if(count == 1) {
+            printf("%d", arr[i]);
+            break;
+        }
+    }
+    
+    return 0;
+}
