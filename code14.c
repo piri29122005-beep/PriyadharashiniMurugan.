f66569bd530d4da6749037a81f82ab128dfbb114
PriyadharshiniMurugan... <piri29122005@gmail.com>
2026-04-15 16:43:38 +0530

    Add files via upload

---
+++ code14.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // Array for numbers 1–9
+    char *words[] = {
+        "", "one", "two", "three", "four",
+        "five", "six", "seven", "eight", "nine"
+    };
+
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            printf("%s\n", words[i]);
+        } 
+        else if (i % 2 == 0) {
+            printf("even\n");
+        } 
+        else {
+            printf("odd\n");
+        }
+    }
+
+    return 0;
+}
